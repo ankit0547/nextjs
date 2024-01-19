a72125ccce52ad9a3185ec84bcc3b7cc8f3c81b4
Ankit sharma <ankit.sharma09547@gmail.com>
2024-01-20 02:28:31 +0530

    input validation improved

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,11 +1,12 @@
 "use client";
-
+import { useFormState } from "react-dom";
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -15,7 +16,8 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
+          {state.message && <p>{state.message}</p>}
           <div className={classes.row}>
             <p>
               <label htmlFor='name'>Your name</label>
